4509e2de3d08e4639fa25d95b3b0ed28c19afa88
Chan <lhc0506@gmail.com>
2022-01-15 09:28:45 +0900

    edit bcrypt

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,5 +1,5 @@
 const User = require("../model/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 exports.showLogin = (req, res, next) => {
